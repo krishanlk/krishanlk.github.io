6c9245ecb49155540fdd88234379dd252b853c47
krishanlk <ict18827@sjp.ac.lk>
2023-06-20 02:30:03 +0530

    PRODUCT PREVIEW add

--- FootGuard/assets/js/main.js
+++ FootGuard/assets/js/main.js
@@ -214,6 +214,80 @@
     }
   });
 
+
+
+
+
+
+
+
+
+
+
+  /**
+   * Gallery Slider
+   */
+  new Swiper('.gallery-slider', {
+    speed: 400,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 30
+      },
+      1200: {
+        slidesPerView: 7,
+        spaceBetween: 30
+      }
+    }
+  });
+
+  /**
+   * Initiate gallery lightbox 
+   */
+  const galleryLightbox = GLightbox({
+    selector: '.gallery-lightbox'
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   /**
    * Testimonials slider
    */
@@ -243,9 +317,25 @@
     }
   });
 
+
+
+    /**
+   * Animation on scroll
+   */
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  }
+  
+
   /**
    * Initiate Pure Counter 
    */
   new PureCounter();
 
 })()
+
